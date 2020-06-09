d4d381a5d89f4bc2932b6d87dfbb5b037294e027
guladishvili-exceed <n.guladishvili.exceedteam@gmail.com>
2020-06-09 15:16:36 +0400

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {v4 as uuidv4} from 'uuid';
+import ListItem from './components/ListItems'
 import "./App.css";
-import CreatetodoList from "./components/CreateToDoListAndDisplay";
 
 class App extends Component {
-  state() {
-    todos: [{}];
-  }
+	state = {
+		isEditMode:false,
+		todos: [],
+	};
+	handleInputValue = (event) => {
+		this.setState({inputValue: event.target.value, id: uuidv4()});
+	}
+	addItem = () => {
+		let {inputValue, todos, id} = this.state
+		this.setState({todos: [...todos, {inputValue, id}]})
+	}
+	deleteItem = (todoID) => {
+		const todos = this.state.todos.filter((item => item.id !== todoID))
+		this.setState({todos})
+	}
+	editItem = () => {
+		this.setState({
+			isEditMode: !this.state.isEditMode
+		})
+	}
 
+	render() {
+		return (
 
-  addTodo = (todo) => {
-    this.setState({
-      todos: [todo,...this.state.todos]
-    })
-  }
-
-  render() {
-
-    return <CreatetodoList onChange={() => this.addTodo()} />;
-  }
+			<ListItem
+				todos={this.state.todos}
+				addItem={this.addItem}
+				handleInputValue = {this.handleInputValue}
+				deleteItem={this.deleteItem}
+				editItem = {this.editItem}/>
+		)
+	}
 }
 
 export default App;
--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
+import {v4 as uuidv4} from "uuid";
+class itemList extends React.Component  {
 
-const ListItem = () => {
-	return (
-		<li>1</li>
-	)
+
+
+		render() {
+		return <div className={"App"}>
+			<div className="App-header">
+				<h2>Welcome to To-Do List App</h2>
+			</div>
+			<input onChange={this.props.handleInputValue} name={''} type='text'/>
+			<button onClick={() => this.props.addItem()} className={'btn btn-primary'}>Add</button>
+			<ul>
+				{this.props.todos.map((item) => <li key={uuidv4()}>
+					<input type={'checkbox'}/>
+					<label>{item.inputValue}</label>
+					<input type={'text'} className={'hidden'}/>
+					<button onClick={() => this.props.deleteItem(item.id)} className={'btn btn-primary'}>Delete</button>
+					<button onClick={() => this.props.editItem()} className={'btn btn-primary'}>Edit</button>
+				</li>)}
+			</ul>
+		</div>
+		};
 }
 
 
-export default ListItem;
+
+export default itemList;
